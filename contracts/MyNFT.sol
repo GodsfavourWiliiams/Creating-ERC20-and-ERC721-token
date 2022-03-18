780c67215d08f4b0cbe2d101b7450fddb458fe69
GodzFeivur <godzfeivvur@gmail.com>
2022-03-18 22:44:26 +0100

    update hard hat config file

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,27 +1,36 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.8.0;
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+//specifies the compiletr version
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//importing the ERC721 contract from openzeppelin
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+//importing the counter to increment my token id
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract NFT is ERC721URIStorage {
+    //extending the properties of the ERC721URIStorage contract to my NFT contract
+
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private tokenId;
 
-    constructor() ERC721("MyNFT", "NFT") {}
+    constructor() ERC721("MaureenNFT", "MNFT") {}
+    //instantiates the constructor fxn of erc721 contract by creating name and symbol for my nft
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
+    function mintNFT (address mintTo, string memory tokenURI) public returns (uint256) {
+        tokenId.increment(); //increments base token ID
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        uint256 newTokenId = tokenId.current(); 
+        //sets the current token id to the variable newTokenId
 
-        return newItemId;
+        _mint(mintTo, newTokenId);
+        //mints NFT to the 'mintTo' address with an NFT id of 'newTokenId
+
+        _setTokenURI(newTokenId, tokenURI);
+        //sets token URI with the new token id and token URI
+
+        return newTokenId;
     }
+  
 }
